3f825d9e64e3e53ac50be16ce8ba932e8bbfd16e
IS Oguro <ingrid.shizue@gmail.com>
2026-01-14 19:58:19 -0300

    Criando endpoints: obter todos e delete.

--- main.py
+++ main.py
@@ -14,10 +14,25 @@ def create_item(item:Item):
     items.append(item)
     return items
 
+# GET ALL ITEMS
+@app.get("/items")
+def get_all():
+    return items
+
 # GET ITEM BY ID
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", response_model=Item)
 def get_item(item_id:int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
         raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+# DELETE
+@app.delete("/items/{item_id}")
+def delete_item(item_id:int) -> list[Item]:
+    if item_id < len(items):
+        items.pop(item_id)
+        return items
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
