0f7dccdfd9db3665f16f030e7cff20eafcf0485b
IS Oguro <ingrid.shizue@gmail.com>
2026-01-14 19:37:01 -0300

    Usando pydantic

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 items = []
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
 
 # POST
 @app.post("/items")
-def create_item(item:str):
+def create_item(item:Item):
     items.append(item)
     return items
 
 # GET ITEM BY ID
 @app.get("/items/{item_id}")
-def get_item(item_id:int) -> str:
+def get_item(item_id:int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
